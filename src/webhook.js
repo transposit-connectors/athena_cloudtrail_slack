1cfdea9bc7de5df579bd222845bc4b924b06832b
Dan Moore <mooreds@transposit.com>
2019-10-08 22:56:43 +0000

    look for phrase

--- src/webhook.js
+++ src/webhook.js
@@ -1,28 +1,38 @@
-/**
- * This operation is an example of a JavaScript operation deployed as a Webhook
- * and configured to work with Slack.
- *
- * For sample code and reference material, visit
- * https://www.transposit.com/docs/building/webhooks
- */
-({ http_event }) => {
+({http_event}) => {
   if (http_event.parsed_body.challenge) {
-  	return {
-    	status_code: 200,
-    	headers: { "Content-Type": "text/plain" },
-    	body: http_event.parsed_body.challenge
-  	};
-	}
+    return {
+      status_code: 200,
+      headers: {
+        "Content-Type": "text/plain"
+      },
+      body: http_event.parsed_body.challenge
+    };
+  }
+  
   console.log("here");
   console.log(http_event.parsed_body);
   const text_we_saw = http_event.parsed_body.event.text;
   const sent_by_bot = http_event.parsed_body.event.bot_id != null;
-  if (!sent_by_bot) {
-  	setImmediate(() => {
-    	api.run("this.post_chat_message",{text: "hello there. I saw: "+text_we_saw});
-  	});
+  
+  if (sent_by_bot) {
+    return {
+      status_code: 200,
+    };
   }
-   return {
+  
+  const check_today = text_we_saw.contains("check today");
+  const check_yesterday = text_we_saw.contains("check yesterday");
+
+  
+  if (!(check_today || check_yesterday)) {
+    setImmediate(() => {
+      api.run("this.post_chat_message", {
+        text: "hello there. I saw: " + text_we_saw +". But I don't understand what to do. Please either ask me to 'check today' or 'check yesterday'";
+      });
+    });
+  }
+  
+  return {
     status_code: 200,
   };
 }
