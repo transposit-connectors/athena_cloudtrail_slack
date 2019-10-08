3982af64fecc6e8c2ef3d59657dd018725c7cfcd
Dan Moore <mooreds@transposit.com>
2019-10-08 20:29:40 +0000

    push to s3 after marketing

--- src/get_log_files.js
+++ src/get_log_files.js
@@ -1,13 +1,19 @@
 (params) => {
   const bucket_name = 'mooreds-cloudtrail';
-  const processed_prefix = 'processed/AWSLogs/425414788231/CloudTrail/us-east-2/2019/10/';
+  const processed_prefix = 'processed/';
   const results = api.run("this.list_objects",{
     bucket_name: bucket_name,
     log_path: 'AWSLogs/425414788231/CloudTrail/' // CloudTrail/us-east-2/2019/10/'
   });
+  
+
+  
+  const ip_address_to_country = {}; // to save on ip calls, we only get 10k
   results.forEach((keyObj) => {
+    const result_records = [];
     //console.log(keyObj.Key);
-    const content = api.query("SELECT * FROM aws_s3.get_object WHERE Bucket=@bucket_name AND Key=@key",{key:keyObj.Key, bucket_name: bucket_name});
+    const key = keyObj.Key;
+    const content = api.query("SELECT * FROM aws_s3.get_object WHERE Bucket=@bucket_name AND Key=@key",{key:key, bucket_name: bucket_name});
     //console.log(content);
     //if (content )
     content.forEach((record_obj) => {
@@ -16,27 +22,40 @@
       //console.log(record_keys);
       record_keys.forEach((rk) => {
         const entry = records[rk];
-        if (entry.eventSource == "s3.amazonaws.com") {return}
-               if (entry.eventSource == "dynamodb.amazonaws.com") {return}
-        if (entry.eventSource == "sns.amazonaws.com") {return}
-        if (entry.eventSource == "autoscaling.amazonaws.com") {return}
-        if (entry.eventSource == "cloudtrail.amazonaws.com") {return}
-        if (entry.eventSource == "monitoring.amazonaws.com") {return}
-        if (entry.eventSource == "kms.amazonaws.com") {return}
-        if (entry.eventSource == "resource-groups.amazonaws.com") {return}
-        if (entry.eventSource == "logs.amazonaws.com") {return}
-        if (entry.eventSource == "config.amazonaws.com") {return}
-        if (entry.eventSource == "lambda.amazonaws.com") {return}
-      console.log("pi: "+entry.userIdentity.principalId);
-      console.log("t: "+entry.eventTime);
-      console.log("sip: "+entry.sourceIPAddress);
-      console.log("en: "+entry.eventName);
-        console.log("es: "+entry.eventSource);
+        if (entry.sourceIPAddress) {
+          const ip = entry.sourceIPAddress;
+          if (ip_address_to_country[ip] === undefined) {
+            const country_code = api.run("this.get_country_from_ip",{ipaddress:ip})[0];
+            ip_address_to_country[ip] = country_code;
+          }
+          if (ip_address_to_country[ip] != 'US') {
+            entry.xpriority = "HIGH";
+          }
+        }
+        if (entry.eventSource == "iam.amazonaws.com") {
+          entry.xpriority = "HIGH";
+        }
+        if (entry.eventSource == "s3.amazonaws.com") {
+          entry.xpriority = "MED";
+        }
+        result_records.push(entry);
       });
-    
+      
     });
+    const result_object = {};
+    result_object.Records = result_records;
+    const processed_key = processed_prefix + key;
+    const res = api.query("SELECT * FROM aws_s3.put_object WHERE Bucket=@bucket_name AND Key=@key AND $body=@body",
+                          {
+      bucket_name: bucket_name,
+      key: processed_key,
+      body: JSON.stringify(result_object);
+    })
     //console.log(content[0]);
   });
+  console.log("here");
+  console.log(ip_address_to_country);
+  console.log("there");
   return results;
 }
 
