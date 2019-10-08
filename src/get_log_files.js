ed39ce99a4cf3eb242da1e036184ce6e64e69f52
Dan Moore <mooreds@transposit.com>
2019-10-08 21:02:17 +0000

    stash values so we don't re-process files we've already seen

--- src/get_log_files.js
+++ src/get_log_files.js
@@ -1,18 +1,20 @@
 (params) => {
   const bucket_name = 'mooreds-cloudtrail';
   const processed_prefix = 'processed/';
+  const stash_suffix = "-processed";
   const results = api.run("this.list_objects",{
     bucket_name: bucket_name,
     log_path: 'AWSLogs/425414788231/CloudTrail/' // CloudTrail/us-east-2/2019/10/'
   });
-  
 
-  
   const ip_address_to_country = {}; // to save on ip calls, we only get 10k
   results.forEach((keyObj) => {
     const result_records = [];
     //console.log(keyObj.Key);
     const key = keyObj.Key;
+    if (stash.get(key+stash_suffix)) {
+      return;
+    }
     const content = api.query("SELECT * FROM aws_s3.get_object WHERE Bucket=@bucket_name AND Key=@key",{key:key, bucket_name: bucket_name});
     //console.log(content);
     //if (content )
@@ -25,11 +27,12 @@
         if (entry.sourceIPAddress) {
           const ip = entry.sourceIPAddress;
           if (ip_address_to_country[ip] === undefined) {
-            const country_code = api.run("this.get_country_from_ip",{ipaddress:ip})[0];
+            const country_code = api.run("this.get_country_from_ip",{ipaddress:ip})[0].country_code;
             ip_address_to_country[ip] = country_code;
           }
           if (ip_address_to_country[ip] != 'US') {
             entry.xpriority = "HIGH";
+            entry.xcountry_code = ip_address_to_country[ip];
           }
         }
         if (entry.eventSource == "iam.amazonaws.com") {
@@ -44,18 +47,26 @@
     });
     const result_object = {};
     result_object.Records = result_records;
-    const processed_key = processed_prefix + key;
+    
+    // can't gzip it just yet
+    const processed_key = (processed_prefix + key).replace(".gz","");
     const res = api.query("SELECT * FROM aws_s3.put_object WHERE Bucket=@bucket_name AND Key=@key AND $body=@body", {
       bucket_name: bucket_name,
       key: processed_key,
       body: JSON.stringify(result_object)
-    })
+    });
+    console.log(res);
+    if (res != "success") {
+      console.log("error processing: "+key);
+    } else {
+      stash.put(key+stash_suffix,true);
+    }
     //console.log(content[0]);
   });
   // console.log("here");
   // console.log(ip_address_to_country);
   // console.log("there");
-  return results;
+  return { status: 200 };
 }
 
 /*
