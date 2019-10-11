92b6c8029a8bef0eab819da20603f380335db80b
Dan Moore <mooreds@transposit.com>
2019-10-11 02:40:27 +0000

    logging

--- src/post_user_query_results_to_slack.js
+++ src/post_user_query_results_to_slack.js
@@ -1,6 +1,8 @@
 (params) => {
   let text = "test";
   const queryId = stash.get("query-id");
+  console.log("here5");
+  console.log(queryId);
   const results = api.run("athena_library.getQueryResults", {queryId: queryId });
    
    api.run("this.post_chat_message", {
