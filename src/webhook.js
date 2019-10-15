7b08e2313b6583ebdaf34fb4d2c5198e5783bbe5
Dan Moore <mooreds@transposit.com>
2019-10-15 23:08:28 +0000

    removed check yesterday, enabled search

--- src/webhook.js
+++ src/webhook.js
@@ -28,7 +28,7 @@
   if (!text_we_saw) {
     setImmediate(() => {
       api.run("this.post_chat_message", {
-        text: "hello there. I saw: " + text_we_saw +". But I don't understand what to do. Please either ask me to 'check today' or 'check yesterday'"
+        text: "hello there. I saw: " + text_we_saw +". But I don't understand what to do. Please either ask me to 'check today'"
       });
     });
   }
@@ -36,7 +36,7 @@
   if (!(check_today || check_yesterday)) {
     setImmediate(() => {
       api.run("this.post_chat_message", {
-        text: "hello there. I saw: " + text_we_saw +". But I don't understand what to do. Please either ask me to 'check today' or 'check yesterday'"
+        text: "hello there. I saw: " + text_we_saw +". But I don't understand what to do. Please either ask me to 'check today'"
       });
     });
   }
@@ -51,21 +51,21 @@
       text: text
     });
     
-    // const athena_output_s3_path = "s3://"+bucket_name + "/" + athena_prefix;
-    // console.log(athena_output_s3_path);
-    // const results = api.run("athena_library.runQuery", {
-    //   query:"select * from default.cloudtrail_enriched where xpriority = 'HIGH' limit 5",                                
-    //   resultlocation: athena_output_s3_path
-    // })[0];
-    // const queryId = results.queryId;
-    // stash.put("query-id", queryId);
-    // console.log(queryId);
+    const athena_output_s3_path = "s3://"+bucket_name + "/" + athena_prefix;
+    console.log(athena_output_s3_path);
+    const results = api.run("athena_library.runQuery", {
+      query:"select * from default.cloudtrail_enriched where xpriority = 'HIGH' limit 5",                                
+      resultlocation: athena_output_s3_path
+    })[0];
+    const queryId = results.queryId;
+    stash.put("query-id", queryId);
+    console.log(queryId);
   });
   
-  // let moment = require('moment-timezone-with-data.js');
-  // let inOneMinute = moment().add(1, "minute").format();
-  // task.create("this.post_user_query_results_to_slack")
-  //   .runOnce(inOneMinute);
+  let moment = require('moment-timezone-with-data.js');
+  let inOneMinute = moment().add(1, "minute").format();
+  task.create("this.post_user_query_results_to_slack")
+    .runOnce(inOneMinute);
   
   return {
     status_code: 200,
