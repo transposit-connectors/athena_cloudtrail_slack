d064b130a621c492472fbea78c356ab397104838
Dan Moore <mooreds@transposit.com>
2019-10-16 03:23:10 +0000

    uncommenting the athena query

--- src/post_user_query_results_to_slack.js
+++ src/post_user_query_results_to_slack.js
@@ -1,12 +1,12 @@
 (params) => {
   let text = "The following high priority events happened recently\n";
-//   const queryId = stash.get("query-id");
-//   // console.log(queryId);
-//   const results = api.run("athena_library.getQueryResults", {queryId: queryId });
+  const queryId = stash.get("query-id");
+  // console.log(queryId);
+  const results = api.run("athena_library.getQueryResults", {queryId: queryId });
   
-//   text += results.map((r) => {
-//     return "event " + r.eventid + " happened at " + r.eventtime + " in region: "+ r.awsregion;
-//   }).join("\n");
+  text += results.map((r) => {
+    return "event " + r.eventid + " happened at " + r.eventtime + " in region: "+ r.awsregion;
+  }).join("\n");
   
   api.run("this.post_chat_message", {
       text: text
