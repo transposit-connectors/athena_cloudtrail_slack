eb49e5ca4a78ef01a9fc25294e923339090d7639
Dan Moore <mooreds@transposit.com>
2019-10-22 14:48:23 +0000

    change to look at event id

--- src/webhook.js
+++ src/webhook.js
@@ -13,7 +13,8 @@
   const athena_prefix = env.get('athena_results_prefix')
   const text_we_saw = http_event.parsed_body.event.text;
   const sent_by_bot = http_event.parsed_body.event.bot_id != null;
-  const error_message = "Hello there. I saw: " + text_we_saw + ". But I don't understand what to do. Please ask me to 'check most recent'"
+  const command_text = "check out"
+  const error_message = "Hello there. I saw: " + text_we_saw + ". But I don't understand what to do. Please ask me to '"+command_text+" [event_id]'";
 
   if (sent_by_bot) {
     return {
@@ -21,9 +22,9 @@
     };
   }
 
-  const check_most_recent = text_we_saw.includes("check most recent");
+  const understood_command = text_we_saw.includes(command_text);
 
-  if ((!text_we_saw) || (!check_most_recent)) {
+  if ((!text_we_saw) || (!understood_command)) {
     setImmediate(() => {
       api.run("this.post_chat_message", {
         text: error_message
@@ -35,8 +36,15 @@
   }
 
   let text = "";
-  if (check_most_recent) {
-    text = "OK, I'll see if there are any recent events.... This may take a minute."
+  let event_id = "";
+  if (understood_command) {
+    const command_args = text_we_saw.split(/ +/);
+    if (command_args.length >=4) {
+      event_id = command_args[3];
+      text = "OK, I'll look at event "+event_id+" .... This may take a minute."
+    } else {
+      text = error_message;
+    }
   }
 
   setImmediate(() => {
@@ -46,7 +54,7 @@
 
     const athena_output_s3_path = "s3://" + bucket_name + "/" + athena_prefix;
     const results = api.run("athena_library.runQuery", {
-      query: "select * from cloudtrail_enriched where xpriority = 'HIGH' order by eventtime desc limit 5",
+      query: "select * from cloudtrail_enriched where eventid = "+event_id,
       resultlocation: athena_output_s3_path
     })[0];
     const queryId = results.queryId;
